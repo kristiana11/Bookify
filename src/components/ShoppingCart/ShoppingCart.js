f18109b4d5468f7917bfb68b2a961f8c9801675d
Kristiana Kirk <93414377+kristiana11@users.noreply.github.com>
2023-08-05 10:37:13 -0600

    checkout feature, but not payment yet

---
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,74 @@
+// import React from 'react';
+// import { FaTimes, FaTrash } from "react-icons/fa";
+// import "./ShoppingCart.css";
+
+// const ShoppingCart = ({ cartItems, removeFromCart, setShowCart }) => {
+//   return (
+//     <div className="cart-container">
+//       <div className="cart-header">
+//         <h2>Checkout</h2>
+//         <div className="close-cart" onClick={() => setShowCart(false)}>
+//           <FaTimes />
+//         </div>
+//       </div>
+//       {cartItems.length ? (
+//         cartItems.map(item => (
+//           <div key={item._id} className="cart-item">
+//             <div className="title">{item.Title}</div>
+//             <div className="author">{item.Author}</div>
+//             <div className="price">{item.Price}</div>
+//             <button className="delete-button" onClick={() => removeFromCart(item._id)}>
+//               <FaTrash />
+//             </button>
+//           </div>
+//         ))
+//       ) : (
+//         <div>Cart is empty</div>
+//       )}
+//       {cartItems.length > 0 && <button className="checkout-button">Checkout</button>}
+//     </div>
+//   );
+// };
+
+// export default ShoppingCart;
+
+import React from 'react';
+import { FaTimes } from "react-icons/fa";
+import './ShoppingCart.css';
+
+const ShoppingCart = ({ cartItems, removeFromCart, setShowCart }) => {
+  const getTotalPrice = () => {
+    const total = cartItems.reduce((sum, item) => sum + parseFloat(item.Price.replace('$', '')), 0);
+    return total.toFixed(2);
+  };
+  
+
+  
+  return (
+    <div className="cart-container">
+      <div className="cart-header">
+        <h2>Shopping Cart</h2>
+        <FaTimes className="close-cart" onClick={() => setShowCart(false)} />
+      </div>
+      {cartItems.length === 0 ? (
+        <div className="cart-empty">Your cart is empty.</div>
+      ) : (
+        <>
+          {cartItems.map((item) => (
+            <div key={item._id} className="cart-item">
+              <div className="title">{item.Title}</div>
+              <div className="price">${item.Price}</div>
+              <button className="delete-button" onClick={() => removeFromCart(item._id)}>
+                <FaTimes />
+              </button>
+            </div>
+          ))}
+          <div className="cart-total">Total: ${getTotalPrice()}</div>
+          <button className="checkout-button">Checkout</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ShoppingCart;
